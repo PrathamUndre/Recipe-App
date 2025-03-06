3a0a2a107fb6017e746ca79ceab750439ea02437
PrathamUndre <undreprathamesh62@gmail.com>
2025-03-07 01:18:19 +0530

    Update navbar.js

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 
 export const Navbar = () => {
   const [cookies, setCookies] = useCookies(["access_token"]);
